140c8651e204845b7ea1a9fcb4ef0fa745e7110b
Ed Cragg <ed.cragg@eipi.xyz>
2023-03-13 23:04:48 +0000

    ...

--- include/treaclePWM.h
+++ include/treaclePWM.h
@@ -1,7 +1,9 @@
 #ifndef __TREACLE_PWM_H__
 #define __TREACLE_PWM_H__
 
-struct treaclePwm {
+struct treaclePWM {
+  treaclePWM(uint8_t pin) : pin(pin) {};
+
   void setPin(int pin);
   void setPeriodMs(uint32_t ms);
   void setFrequency();
@@ -17,10 +19,8 @@ private:
   bool inverted = false;
   bool running = false;
   uint32_t timeout;
+  uint32_t timeoutHigh;
+  uint8_t pin;
 };
-
-struct treaclePwmCallback {
-
-}
 
 #endif // __TREACLE_PWM_H__
