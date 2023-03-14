6bbf5912451c0d7bb0fe2a8fc1c00851b18c4d16
Ed Cragg <ed.cragg@eipi.xyz>
2023-03-14 08:17:19 +0000

    Changes to main

--- src/main.cpp
+++ src/main.cpp
@@ -6,17 +6,11 @@ treaclePWM tpwm(6);
 void ping() { tpwm.ping(); }
 
 void setup() {
-  Serial.begin(115200);
   Timer1.initialize(5000);
   Timer1.attachInterrupt(ping);
   tpwm.setFrequency(5);
   tpwm.setDutyCycle(0.75);
-  tpwm.setFrequency(8.0);
-  tpwm.setDutyCycle(0.1);
-  tpwm.setDutyCycleByte((uint8_t)127);
   tpwm.start();
 }
 
-void loop() {
-  tpwm.ping();
-}
+void loop() {}
